da2b36c7bbbe1658d5c986ea2915c6c6e4ca6deb
katjam <katja@neontribe.co.uk>
2014-07-18 09:29:12 +0100

    card #3 Added contextual help text to dropdown for post edit.

--- dil-post-edit-help-txt.php
+++ dil-post-edit-help-txt.php
@@ -6,34 +6,40 @@ Author: Neontribe
 Author URI: http://www.neontribe.co.uk
 */ 
 
-// Add metabox below editing pane
- function dil_metabox_after_title() {
+/**
+* Add metabox below editing pane
+*/
+function dil_metabox_after_title() {
 	    add_meta_box( 'after-title-help', 'Tips for writing your story', 'dil_after_title_help_metabox_content', 'post', 'advanced', 'high' );
 } 
 add_action( 'add_meta_boxes', 'dil_metabox_after_title' );
 
-// callback function to populate metabox
+/**
+ * callback function to populate metabox
+ */
 function dil_after_title_help_metabox_content() { ?> 
-	    <p>
-				 Use this form to add your story.
-				 
-				 Follow these tips for the best chance of getting published.
+<p>
+  Use this form to add your story.
 
-				 <ol>
-				 <li>Keep below the word count of 700 words.</li>
-				 <li>Don't use inappropriate language</li>
-				 </ol>
-			</p>
-<?php }  
+  Follow these tips for the best chance of getting published.
+  <ol>
+    <li>Keep below the word count of 700 words.</li>
+    <li>Don't use inappropriate language</li>
+  </ol>
+</p>
+<?php }
 
-// Add help text to right of screen in a metabox
+/**
+ * Add help text to right of screen in a metabox
+ */
 function dil_metabox_top_right() {
 	add_meta_box( 'above-publish-help', 'Publishing and Saving Changes', 'dil_top_right_help_metabox_content', 'post', 'side', 'high' );
 }
 add_action( 'add_meta_boxes', 'dil_metabox_top_right' );
 
-
-// callback function to populate metabox
+/**
+ * callback function to populate metabox
+ */
 function dil_top_right_help_metabox_content() { ?>
 	<p>
 		 Make sure you click 'Submit for review' below once you've finished your story, to send it to our moderators for publishing. 
@@ -42,6 +48,45 @@ function dil_top_right_help_metabox_content() { ?>
 	</p>
 <?php }
 
+/**
+ * Add contextual help tab to post edit screen
+ * 
+ */
+function dil_contextual_help( $help_text, $screen_id, $screen ) {
+	switch ($screen_id) {
+	  case 'post':
+		  $screen->remove_help_tabs(); // To remove wordpress defaults
+			$screen->add_help_tab( array(
+			  'id' => 'guidelines',
+				'title' => 'Day in the Life guidelines',
+				'content' => '', // From callback
+				'callback' => 'dil_post_help',
+			));
+			$screen->add_help_tab( array(
+			  'id' => 'other-info',
+				'title' => 'Day in the Life Information',
+				'content' => '<p>Important information about posting. Can be made specific to user type - admin or contributor.</p>', // From callback
+			));
+
+			$screen->set_help_sidebar(
+			  '<p>Information and links here...</p>'
+			);
+
+	    return;
+
+	  default:
+		  return;
+	}
+}
+
+/**
+ * Callback - Help text for post pages
+ */
+function dil_post_help() {
+  echo '<h3>Guidelines for writing a post</h3>';
+	echo '<p>Your guidelines here...</p>';
+}
+add_action( 'contextual_help', 'dil_contextual_help', 5, 3);
 
 //code.tutsplus.com/articles/customizing-the-wordpress-admin-help-text--wp-33281
 ?>
